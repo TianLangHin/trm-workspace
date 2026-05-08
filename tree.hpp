b5cda3208afa2ff86dcb6f32f48e49b5a3787db4
Tian Lang Hin <tianlanghin@gmail.com>
2026-05-08 21:45:30 +1000

    Completed implementation for tree node using smart pointers

--- tree.hpp
+++ tree.hpp
@@ -11,9 +11,68 @@ private:
     int merit;
     bool player;
 
-    std::optional<std::array<std::shared_ptr<TreeNode<Width>>, Width>> children;
-    std::weak_ptr<TreeNode<Width>> parentPtr;
-    std::weak_ptr<TreeNode<Width>> nextPtr;
+    // Modern C++ Lesson 1: Improved type declarations (`using` rather than `typedef`).
+    using SharedPtr = std::shared_ptr<TreeNode<Width>>;
+    using WeakPtr = std::weak_ptr<TreeNode<Width>>;
+
+    // C++ Development Lesson 5: Using an `optional`, since the array must be of size `Width` or be empty.
+    std::optional<std::array<SharedPtr, Width>> children;
+    WeakPtr parentPtr;
+    WeakPtr nextPtr;
+
+public:
+    // Modern C++ Lesson 3: Uniform initialisation syntax used in the constructor.
+    TreeNode(int depth, bool player): merit {0}, player {player}, parentPtr {}, nextPtr{} {
+        if (depth <= 0) {
+            this->children = {};
+        } else {
+            this->children = std::array<SharedPtr, Width> {};
+            for (auto& child : this->children.value()) {
+                child = std::make_shared<TreeNode<Width>>(TreeNode(depth - 1, !player));
+            }
+            for (int i = 0; i < Width - 1; ++i) {
+                this->children.value()[i]->nextPtr = WeakPtr(this->children.value()[i+1]);
+            }
+        }
+    }
+
+    std::optional<SharedPtr> parent() {
+        // Modern C++ Lesson 4: Checking weak pointer object validity status.
+        if (parentPtr.expired()) {
+            // C++ Development Lesson 5: empty initialisation constructs `nullopt` for optional.
+            return {};
+        }
+        // Modern C++ Lesson 4: Converting weak pointer to shared pointer.
+        return parentPtr.lock();
+    }
+
+    std::optional<SharedPtr> first() {
+        // C++ Development Lesson 5: Using `has_value` and `value()`
+        // to check/retrieve possibly empty values from an optional.
+        if (!children.has_value()) {
+            return {};
+        }
+        return children.value()[0];
+    }
+
+    std::optional<SharedPtr> next() {
+        if (nextPtr.expired()) {
+            return {};
+        }
+        return nextPtr.lock();
+    }
+
+    std::optional<std::array<SharedPtr, Width>> getChildren() {
+        return children;
+    }
+
+    int getMerit() {
+        return merit;
+    }
+
+    bool getPlayer() {
+        return player;
+    }
 }
 
 #endif
