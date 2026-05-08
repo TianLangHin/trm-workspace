f3a5e61f7657eb8749952105e3786967e68a8d1d
Tian Lang Hin <tianlanghin@gmail.com>
2026-05-08 23:34:14 +1000

    Added driver code for running the SSS* algorithm

--- main.cpp
+++ main.cpp
@@ -2,36 +2,61 @@
 #include <memory>
 #include <vector>
 
+#include "alpha_beta.hpp"
 #include "computation.hpp"
+#include "heuristics.hpp"
+#include "sss.hpp"
 #include "tree.hpp"
 
+#ifndef TREE_WIDTH
+#define TREE_WIDTH 2
+#endif
+
 int main() {
-    std::cout << "SSS* simulator\n";
+    constexpr int Width = TREE_WIDTH;
+    static_assert(Width >= 2, "Tree width must be 2 or more.");
 
-    Case c1 { Case1 {} };
-    Case c2 { Case2 {} };
-    std::cout << "Two cases:" << c1.name() << ' ' << c2.name() << '\n';
+    int depth;
+    std::cout << "Tree width is " << Width << ". Enter depth of tree: ";
+    std::cin >> depth;
+    std::cout << '\n';
 
-    std::shared_ptr<TreeNode<3>> tree = std::make_shared<TreeNode<3>>(TreeNode<3>(2, true));
-    std::vector<int> merits { 9, 6, 3, 8, 5, 2, 7, 4, 1 };
-    auto it = merits.begin();
-    tree->fill_with_values(it, merits.begin(), merits.end());
+    int limit = std::numeric_limits<int>::max();
 
-    auto n1 = tree->first().value();
-    auto n2 = n1->next().value();
-    auto n3 = n2->next().value();
+    auto [bestValues, worstValues] = heuristics(Width, depth);
 
-    std::cout << "n11:" << n1->first().value()->getMerit() << '\n';
-    std::cout << "n12:" << n1->first().value()->next().value()->getMerit() << '\n';
-    std::cout << "n13:" << n1->first().value()->next().value()->next().value()->getMerit() << '\n';
+    std::vector<Case> bestPath {};
+    auto bestNode = std::make_shared<TreeNode<Width>>(TreeNode<Width>(depth, true));
+    auto bestIt = bestValues.begin();
+    bestNode->fill_with_values(bestIt, bestValues.begin(), bestValues.end());
+    int sssLeafBest = 0, abLeafBest = 0;
+    int sssBest = sss(bestNode, bestPath, sssLeafBest);
+    int abBest = alpha_beta(bestNode, -limit, limit, abLeafBest);
 
-    std::cout << "n21:" << n2->first().value()->getMerit() << '\n';
-    std::cout << "n22:" << n2->first().value()->next().value()->getMerit() << '\n';
-    std::cout << "n23:" << n2->first().value()->next().value()->next().value()->getMerit() << '\n';
+    std::vector<Case> worstPath {};
+    auto worstNode = std::make_shared<TreeNode<Width>>(TreeNode<Width>(depth, true));
+    auto worstIt = worstValues.begin();
+    worstNode->fill_with_values(worstIt, worstValues.begin(), worstValues.end());
+    int sssLeafWorst = 0, abLeafWorst = 0;
+    int sssWorst = sss(worstNode, worstPath, sssLeafWorst);
+    int abWorst = alpha_beta(worstNode, -limit, limit, abLeafWorst);
 
-    std::cout << "n31:" << n3->first().value()->getMerit() << '\n';
-    std::cout << "n32:" << n3->first().value()->next().value()->getMerit() << '\n';
-    std::cout << "n33:" << n3->first().value()->next().value()->next().value()->getMerit() << '\n';
+    std::cout << "Best-Case SSS* Computation Path:\n";
+    for (auto c : bestPath) {
+        std::cout << c.name() << ' ';
+    }
+    std::cout << '\n';
 
-    return 0;
+    std::cout << "Worst-Case SSS* Computation Path:\n";
+    for (auto c : worstPath) {
+        std::cout << c.name() << ' ';
+    }
+    std::cout << '\n';
+
+    std::cout << "Best-Case:\n";
+    std::cout << "SSS* Result: " << sssBest << ", Alpha-Beta Result: " << abBest << '\n';
+    std::cout << "SSS* Leaf Count: " << sssLeafBest << ", Alpha-Beta Leaf Count: " << abLeafBest << '\n';
+    std::cout << "Worst-Case:\n";
+    std::cout << "SSS* Result: " << sssWorst << ", Alpha-Beta Result: " << abWorst << '\n';
+    std::cout << "SSS* Leaf Count: " << sssLeafWorst << ", Alpha-Beta Leaf Count: " << abLeafWorst << '\n';
 }
